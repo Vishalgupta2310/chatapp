b966735f951b8e39c2cd2db3754e07e95d6e30fc
Vishal Gupta <102800119+Vishalgupta2310@users.noreply.github.com>
2022-04-02 01:26:08 +0530

    char.php

---
+++ chat.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realtime Chat App | By Vishal Gupta</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <selection class="chat-area">
+            <header>
+                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="my.jpeg" alt="">
+                <div class="details">
+                    <span>Coding Vishal</span>
+                    <p>Active Now</p>
+                </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, conssddgdddddddddddddddddddsssssssectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="my.jpeg" alt="">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, consectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, conssddgdddddddddddddddddddsssssssectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="my.jpeg" alt="">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, consectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, conssddgdddddddddddddddddddsssssssectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="my.jpeg" alt="">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, consectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, conssddgdddddddddddddddddddsssssssectur adisplingg wlit</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="my.jpeg" alt="">
+                    <div class="details">
+                        <p>lorem ipsum sit amot, consectur adisplingg wlit</p>
+                    </div>
+                </div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" placeholder="Type a message here...">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </selection>
+    </div>
+</body>
+
+</html>
