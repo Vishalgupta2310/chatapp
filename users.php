ee24b7d45783ee219c13990d9729d651633c7fda
Vishal Gupta <102800119+Vishalgupta2310@users.noreply.github.com>
2022-04-01 19:48:32 +0530

    Add files via upload

---
+++ users.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realtime Chat App | By Vishal Gupta</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <selection class="users">
+            <header>
+                <div class="content">
+                <img src="my.jpeg" alt="">
+                <div class="details">
+                    <span>Coding Vishal</span>
+                    <p>Active Now</p>
+                </div>
+                </div>
+                <a href="#" class="logout">Logout</a>
+            </header>
+            <div class="search">
+                <span class="text">Select an user to strat chat</span>
+                <input type="text" placeholder="Enter name to search...">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+        </selection>
+    </div>
+</body>
+
+</html>
